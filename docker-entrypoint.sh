e32a729f5370c1727de401286fa1db8ea930442b
pfzoso55 <58383054+pfzoso55@users.noreply.github.com>
2020-09-26 01:29:41 -0500

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# we need to tell GUI applications which display to use
+export DISPLAY=xserver:0
+
+# we also need to use the proper magic cookie for authorization
+touch ~/.Xauthority
+xauth merge /Xauthority/xserver.xauth
+
+exec "$@"
